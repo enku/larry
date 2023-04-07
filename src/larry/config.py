49f4bfb09e415680bbcf6a87ccb5b983cf3e412d
Albert Hopkins <marduk@letterboxes.org>
2023-04-06 21:28:32 -0500

    config: allow toml-formated config files

--- src/larry/config.py
+++ src/larry/config.py
@@ -1,7 +1,10 @@
 """Utils dealing with the larry configuration (file)"""
 import configparser
 import importlib.resources
+import io
 import os.path
+import tomllib
+from typing import Any
 
 import platformdirs
 
@@ -17,7 +20,11 @@ def load(path: str) -> configparser.ConfigParser:
     """Return ConfigParser instance given the path"""
     config = configparser.ConfigParser()
     config["DEFAULT"]["input"] = DEFAULT_INPUT_PATH
-    config.read(path)
+
+    try:
+        config.read(path)
+    except configparser.ParsingError:
+        return load_toml_config(path)
 
     return config
 
@@ -33,3 +40,60 @@ def get_plugin_config(plugin_name: str, config_path: str) -> ConfigType:
         plugin_config = config[plugin_config_name] = config["DEFAULT"]
 
     return plugin_config
+
+
+def load_toml_config(path: str) -> configparser.ConfigParser:
+    """Return the TOML config file as a ConfigParser object"""
+    with open(path, "rb") as config_fp:
+        toml = tomllib.load(config_fp)
+
+    return config_from_toml(toml)
+
+
+def config_from_toml(toml: dict[str, Any]) -> configparser.ConfigParser:
+    """Return the dict as a ConfigParser object"""
+    toml = toml.copy()
+    config_io = io.StringIO()
+
+    sep = ""
+
+    for title, table in toml.items():
+        config_io.write(sep)
+        config_io.write(str_from_table(title, table))
+        sep = "\n"
+
+    config = configparser.ConfigParser()
+    config["DEFAULT"]["input"] = DEFAULT_INPUT_PATH
+
+    config.read_string(config_io.getvalue())
+
+    return config
+
+
+def str_from_table(name: str, values: dict[str, Any]) -> str:
+    """Given the key/value dict and table name, return equivalent INI string"""
+    table_io = io.StringIO()
+    table_io.write(f"[{name}]\n")
+
+    for key, value in values.items():
+        if isinstance(value, dict):
+            table_io.write("\n")
+            table_io.write(str_from_table(f"{name}.{key}", value))
+        else:
+            table_io.write(f"{key} = {str_from_config_value(value)}\n")
+
+    return table_io.getvalue()
+
+
+def str_from_config_value(value: Any) -> str:
+    """Return the given Python value as an INI value
+
+    Takes care of converting a list into newline-delimited sting values.
+    """
+    if isinstance(value, str):
+        return value
+
+    if isinstance(value, list):
+        return "\n    ".join(str_from_config_value(item) for item in value)
+
+    return repr(value)
