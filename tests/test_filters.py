73104e6e47670bd02f03ecaec16fada4c8f20acf
Albert Hopkins <marduk@letterboxes.org>
2024-01-25 12:56:03 -0600

    add tests for filters

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,6 +2,7 @@
 import random
 from configparser import ConfigParser
 from copy import deepcopy
+from typing import Any
 from unittest import mock
 
 from larry import filters
@@ -10,10 +11,62 @@ from larry.config import DEFAULT_INPUT_PATH
 from . import TestCase, make_colors
 
 
-class NewImageColorsTests(TestCase):
+def make_config(name: str, **settings: Any) -> ConfigParser:
     config = ConfigParser()
-    config.add_section("filters:test")
-    config["filters:test"]["image"] = DEFAULT_INPUT_PATH
+    section = f"filters:{name}"
+    config.add_section(section)
+
+    for key, value in settings.items():
+        config[section][key] = str(value)
+
+    return config
+
+
+class DarkenTests(TestCase):
+    def test(self):
+        orig_colors = make_colors("#00dd00 #ffde7f #0000ff #ffc0cb")
+        config = make_config("darken", image=DEFAULT_INPUT_PATH, opacity=0.7)
+
+        new_colors = filters.darken(orig_colors, config)
+
+        self.assertEqual(new_colors, make_colors("#004200 #606752 #0000ff #938184"))
+
+
+class LightenTests(TestCase):
+    def test(self):
+        orig_colors = make_colors("#00dd00 #ffde7f #0000ff #ffc0cb")
+        config = make_config("lighten", image=DEFAULT_INPUT_PATH, opacity=0.7)
+
+        new_colors = filters.lighten(orig_colors, config)
+
+        self.assertEqual(new_colors, make_colors("#00dd00 #ffde7f #b2b2ff #ffc0ca"))
+
+
+class GetOpacityTests(TestCase):
+    config = make_config("test", opacity=0.5, foo=0.7, bar=7.0)
+
+    def test(self):
+        result = filters.get_opacity(self.config, "test")
+
+        self.assertEqual(result, 0.5)
+
+    def test_fallback(self):
+        result = filters.get_opacity(self.config, "test", "bogus")
+
+        self.assertEqual(result, 1.0)
+
+    def test_custom_name(self):
+        result = filters.get_opacity(self.config, "test", "foo")
+
+        self.assertEqual(result, 0.7)
+
+    def test_bad_value(self):
+        with self.assertRaises(filters.FilterError):
+            filters.get_opacity(self.config, "test", "bar")
+
+
+class NewImageColorsTests(TestCase):
+    config = make_config("test", image=DEFAULT_INPUT_PATH)
 
     def test(self):
         colors = filters.new_image_colors(6, self.config, "test")
