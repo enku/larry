df6b85bf69a0b339fb63cb21c7c4b1438d32a5ec
Albert Hopkins <marduk@letterboxes.org>
2024-01-28 07:35:01 -0600

    plugins.gtk: grab colors from the input template
    Take advantage of colors.COLOR_RE and collect the colors from the input
    template instead of having to manually specify colors in the config.

--- src/larry/plugins/gtk.py
+++ src/larry/plugins/gtk.py
@@ -1,29 +1,22 @@
 """Larry plugin for gtk"""
 from larry import Color, ColorList
-from larry.color import replace_string2
+from larry.color import COLORS_RE, replace_string2
 from larry.config import ConfigType
 from larry.io import read_file, write_file
 
 
 def plugin(colors: ColorList, config: ConfigType):
     """gtk.css plugin"""
-    theme_color = next(Color.generate_from(colors, 1, randomize=False))
     template = config["template"]
     outfile = config["location"]
-    config_colors_strs = config.get("colors", "").split()
-    config_colors = []
-
-    for string in config_colors_strs:
-        if len(string) == 6:
-            config_colors.append(Color(f"#{string}"))
-        elif string.count(",") >= 2:
-            r, g, b = string.split(",")[:3]
-            config_colors.append(Color(int(r.strip()), int(g.strip()), int(b.strip())))
-
     orig_css = read_file(template).decode()
-    new_css = orig_css
+    orig_colors = list(set(COLORS_RE.findall(orig_css)))
+    theme_colors = list(Color.generate_from(colors, len(orig_colors)))
 
-    colormap = {color: color.colorify(theme_color) for color in config_colors}
+    colormap = {
+        color: theme_color.colorify(color)
+        for color, theme_color in zip(orig_colors, theme_colors)
+    }
     new_css = replace_string2(orig_css, colormap)
 
     write_file(outfile, new_css.encode())
