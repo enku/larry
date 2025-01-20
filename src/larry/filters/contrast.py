55892a1797265c961f3f1dc8d3d207f3c63a40da
Albert Hopkins <marduk@letterboxes.org>
2025-01-20 10:56:15 -0600

    filters.contrast: reduce intermediate variables

--- src/larry/filters/contrast.py
+++ src/larry/filters/contrast.py
@@ -7,13 +7,6 @@ from larry import ColorList
 
 def cfilter(orig_colors: ColorList, _config: ConfigParser) -> ColorList:
     """The darks are so dark and the brights are so bright"""
-    num_colors = len(orig_colors)
-    step = 255 / num_colors
+    step = 255 / len(orig_colors)
 
-    colors = []
-    lum = 0.0
-    for color in orig_colors:
-        colors.append(color.luminize(lum))
-        lum += step
-
-    return colors
+    return [color.luminize(i * step) for i, color in enumerate(orig_colors)]
