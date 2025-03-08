b6337575294f22f97560892774979ca256d00926
Albert Hopkins <marduk@letterboxes.org>
2025-03-08 07:49:34 -0600

    __init__: remove reference to ConfigType
    It has been deprecated for some time.

--- src/larry/__init__.py
+++ src/larry/__init__.py
@@ -2,8 +2,6 @@
 
 import logging
 from importlib.metadata import distribution
-from typing import Any
-from warnings import warn
 
 from larry.color import Color, ColorList
 
@@ -11,17 +9,3 @@ __all__ = ("__version__", "LOGGER", "Color", "ColorList")
 __version__ = distribution("larry").version
 
 LOGGER = logging.getLogger("larry")
-
-
-def __getattr__(name: str) -> Any:
-    if name == "ConfigType":
-        warn(
-            "larry.ConfigType is deprecated. Use larry.config.ConfigType",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        import larry.config  # pylint: disable=import-outside-toplevel
-
-        return larry.config.ConfigType
-
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
