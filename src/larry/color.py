1d436dab3bf83778d7f9f5e51df118d92df11b84
Albert Hopkins <marduk@letterboxes.org>
2025-08-17 16:33:53 -0500

    color: closest: compute distance w/o numpy

--- src/larry/color.py
+++ src/larry/color.py
@@ -6,10 +6,9 @@ import random
 import re
 from collections import namedtuple
 from dataclasses import dataclass
-from math import floor
+from math import floor, sqrt
 from typing import Callable, Iterable, Iterator, Optional, TypeAlias, TypeVar, Union
 
-import numpy as np
 from sklearn.cluster import KMeans
 
 from larry import utils
@@ -488,11 +487,17 @@ class Color(namedtuple("Color", ["red", "green", "blue"])):
 
         return Color.from_hsv((hsv[0], utils.clip(factor * hsv[1]), hsv[2]))
 
+    def distance(self, other: Color) -> float:
+        """Return the distance between two colors"""
+        return sqrt(
+            (self.red - other.red) ** 2
+            + (self.green - other.green) ** 2
+            + (self.blue - other.blue) ** 2
+        )
+
     def closest(self, colors: ColorList) -> Color:
         """Return the closest color given the list of colors"""
-        target = np.array(self)
-
-        return min(colors, key=lambda c: np.linalg.norm(np.array(c) - target))
+        return min(colors, key=self.distance)
 
 
 ColorList: TypeAlias = list[Color]
