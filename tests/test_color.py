6a86a7ee429fcce476a12d8e82358912dd744619
Albert Hopkins <marduk@letterboxes.org>
2025-08-26 20:36:39 -0500

    tests: color: use the random fixture instead of patching

--- tests/test_color.py
+++ tests/test_color.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring,duplicate-code,unused-argument
-import random
-from unittest import TestCase, mock
+from unittest import TestCase
 
 from unittest_fixtures import Fixtures, given
 
@@ -28,8 +27,9 @@ class ColorReTests(TestCase):
         self.assertEqual(colors, expected)
 
 
+@given(lib.random)
 class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
-    def test_is_gray(self):
+    def test_is_gray(self, fixtures: Fixtures) -> None:
         self.assertTrue(Color("#bfbfbf").is_gray())
         self.assertTrue(Color("#ffffff").is_gray())
         self.assertTrue(Color("#666666").is_gray())
@@ -37,87 +37,81 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertFalse(Color("red").is_gray())
         self.assertFalse(Color("#bfbfbd").is_gray())
 
-    def test_is_gray_with_threshold(self):
+    def test_is_gray_with_threshold(self, fixtures: Fixtures) -> None:
         self.assertTrue(Color("#bfbfbd").is_gray(threshold=2))
         self.assertFalse(Color("#bfbfbd").is_gray(threshold=1))
         self.assertFalse(Color("#bfbebd").is_gray(threshold=1))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_with_no_args_is_random(self):
+    def test_init_with_no_args_is_random(self, fixtures: Fixtures) -> None:
         c = Color()
 
         self.assertEqual(c, Color("#442082"))
 
-    def test_init_values_out_of_range(self):
+    def test_init_values_out_of_range(self, fixtures: Fixtures) -> None:
         with self.assertRaises(ValueError):
             Color(255, 255, 300)
 
-    def test_init_from_wrong_number_of_args(self):
+    def test_init_from_wrong_number_of_args(self, fixtures: Fixtures) -> None:
         with self.assertRaises(color.BadColorSpecError):
             Color(255, 255)
 
-    def test_init_from_color(self):
+    def test_init_from_color(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
 
         self.assertEqual(Color(c), c)
 
-    def test_init_bad_colorspec(self):
+    def test_init_bad_colorspec(self, fixtures: Fixtures) -> None:
         with self.assertRaises(color.BadColorSpecError):
             Color(None)
 
-    def test_from_rgb_string(self):
+    def test_from_rgb_string(self, fixtures: Fixtures) -> None:
         s = "rgb(100, 20, 30)"
 
         self.assertEqual(Color(s), Color(100, 20, 30))
 
-    def test_from_rgba_string(self):
+    def test_from_rgba_string(self, fixtures: Fixtures) -> None:
         s = "rgba(100, 20, 30, 0.4)"
 
         self.assertEqual(Color(s), Color(100, 20, 30))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_from_random(self):
+    def test_init_from_random(self, fixtures: Fixtures) -> None:
         c = Color("random")
 
         self.assertEqual(c, Color("#442082"))
 
-    def test_init_string_slashes(self):
+    def test_init_string_slashes(self, fixtures: Fixtures) -> None:
         c = Color("100/20/30")
 
         self.assertEqual(c, Color(100, 20, 30))
 
-    def test_init_string_braces(self):
+    def test_init_string_braces(self, fixtures: Fixtures) -> None:
         c = Color("{1.0, 0.2, 0.3}")
 
         self.assertEqual(c, Color(255, 51, 76))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_random_with_lum(self):
+    def test_init_random_with_lum(self, fixtures: Fixtures) -> None:
         c = Color("random(34)")
 
         self.assertEqual(c, Color("#2b1452"))
         self.assertEqual(round(c.luminocity()), 34)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_random_with_lum_gt(self):
+    def test_init_random_with_lum_gt(self, fixtures: Fixtures) -> None:
         c = Color("random(>34)")
 
         self.assertEqual(c, Color("#52279c"))
         self.assertGreater(round(c.luminocity()), 34)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_random_with_lum_gte(self):
+    def test_init_random_with_lum_gte(self, fixtures: Fixtures) -> None:
         c = Color("random(>=34)")
 
         self.assertEqual(c, Color("#51269a"))
         self.assertGreaterEqual(round(c.luminocity()), 34)
 
-    def test_init_random_with_lum_error(self):
+    def test_init_random_with_lum_error(self, fixtures: Fixtures) -> None:
         with self.assertRaises(color.BadColorSpecError):
             Color("random(==34)")
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_init_from_randhue(self):
+    def test_init_from_randhue(self, fixtures: Fixtures) -> None:
         c = Color("randhue(50, 20)")
 
         self.assertEqual(c, Color("#2f3319"))
@@ -125,7 +119,7 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertEqual(int(s), 50)
         self.assertEqual(int(v), 20)
 
-    def test_from_rrggbb_string(self):
+    def test_from_rrggbb_string(self, fixtures: Fixtures) -> None:
         c = Color(50, 73, 171)
 
         self.assertEqual(Color("#3249ab"), c)
@@ -134,24 +128,24 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertEqual(Color("#cab"), Color("#ccaabb"))
         self.assertEqual(Color("cab"), Color("#ccaabb"))
 
-    def test_bad_init_string_raises_error(self):
+    def test_bad_init_string_raises_error(self, fixtures: Fixtures) -> None:
         with self.assertRaises(color.BadColorSpecError):
             Color("bogus")
 
-    def test_add_float(self):
+    def test_add_float(self, fixtures: Fixtures) -> None:
         c1 = Color("#3249ab")
         c2 = c1 + 20.0
 
         self.assertEqual(c2, Color("#465dbf"))
 
-    def test_add_color(self):
+    def test_add_color(self, fixtures: Fixtures) -> None:
         c1 = Color("#3249ab")
         c2 = Color("#040404")
         c3 = c1 + c2
 
         self.assertEqual(c3, Color("#36af4d"))
 
-    def test_mul_float(self):
+    def test_mul_float(self, fixtures: Fixtures) -> None:
         c1 = Color("#3249ab")
         c2 = c1 * 0.3
 
@@ -161,7 +155,7 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
 
         self.assertEqual(c2, Color("#415ede"))
 
-    def test_mul_color(self):
+    def test_mul_color(self, fixtures: Fixtures) -> None:
         # I don't particularly see the point in this. Maybe remove
         c1 = Color("#3249ab")
         c2 = Color("#040404")
@@ -169,21 +163,21 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
 
         self.assertEqual(c3, Color("#c8ffff"))
 
-    def test_sub(self):
+    def test_sub(self, fixtures: Fixtures) -> None:
         c1 = Color("#ffa500")
         c2 = Color("#3249ab")
         c3 = c1 - c2
 
         self.assertEqual(c3, Color("#ff00a5"))
 
-    def test_colorify(self):
+    def test_colorify(self, fixtures: Fixtures) -> None:
         c1 = Color("#3249ab")
         c2 = Color("#ffa500")
         c3 = c1.colorify(c2)
 
         self.assertEqual(c3, Color("#aa8031"))
 
-    def test_colorify_fix_bw(self):
+    def test_colorify_fix_bw(self, fixtures: Fixtures) -> None:
         c1 = Color("#ffffff")
         c2 = Color("#ffa500")
         c3 = c1.colorify(c2, fix_bw=True)
@@ -195,12 +189,10 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
 
         self.assertEqual(c3, Color("#010101"))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_randcolor(self):
+    def test_randcolor(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color.randcolor(), Color("#442082"))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_randcolor_with_lum(self):
+    def test_randcolor_with_lum(self, fixtures: Fixtures) -> None:
         c = Color.randcolor(lum=34)
         self.assertEqual(c, Color("#2b1452"))
         self.assertEqual(round(c.luminocity()), 34)
@@ -224,34 +216,34 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         with self.assertRaises(color.BadColorSpecError):
             Color.randcolor(lum=34, comp="lt")
 
-    def test_inverse(self):
+    def test_inverse(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
         inverse = c.inverse()
 
         self.assertEqual(inverse, Color("#cdb654"))
 
-    def test_winverse(self):
+    def test_winverse(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
         winverse = c.winverse()
 
         self.assertEqual(winverse, Color("#19b600"))
 
-    def test_to_gtk(self):
+    def test_to_gtk(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
 
         self.assertEqual(c.to_gtk(), "0.20, 0.29, 0.67")
 
-    def test_to_ansi256(self):
+    def test_to_ansi256(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
 
         self.assertEqual(c.to_ansi256(), 61)
 
-    def test_to_ansi256_gray(self):
+    def test_to_ansi256_gray(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color("#acacac").to_ansi256(), 248)
         self.assertEqual(Color("#000000").to_ansi256(), 16)
         self.assertEqual(Color("#ffffff").to_ansi256(), 231)
 
-    def test_pastelize(self):
+    def test_pastelize(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
         p = Color("#7f97ff")
 
@@ -262,7 +254,7 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertEqual(round(s), color.PASTEL_SATURATION)
         self.assertEqual(round(v), color.PASTEL_BRIGHTNESS)
 
-    def test_pastelize_custom(self):
+    def test_pastelize_custom(self, fixtures: Fixtures) -> None:
         c = Color("#3249ab")
         s = 134
         v = 90
@@ -271,13 +263,13 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertEqual(p, Color("#0000e5"))
         self.assertEqual(p, Color.from_hsv((c.to_hsv()[0], s, v)))
 
-    def test_luminize(self):
+    def test_luminize(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color("#0e118f").luminize(70), Color("#2128ff"))
 
-    def test_luminize_black(self):
+    def test_luminize_black(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color("#000000").luminize(70), Color("#464646"))
 
-    def test_gradient(self):
+    def test_gradient(self, fixtures: Fixtures) -> None:
         start, stop = lib.make_colors("#000000 #ffffff")
         steps = 7
 
@@ -288,27 +280,26 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         )
         self.assertEqual(colors, expected)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_generate_from_with_same_number_of_colors(self):
+    def test_generate_from_with_same_number_of_colors(self, fixtures: Fixtures) -> None:
         orig_colors = lib.make_colors(
             "#7e118f #754fc7 #835d75 #807930 #9772ea #9f934b #39e822 #35dfe9"
         )
         colors = list(Color.generate_from(orig_colors, len(orig_colors)))
         self.assertEqual(colors, orig_colors)
 
-    def test_generate_from_with_negatative_needed(self):
+    def test_generate_from_with_negatative_needed(self, fixtures: Fixtures) -> None:
         with self.assertRaises(ValueError):
             list(Color.generate_from([], -1))
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_generate_from_when_no_colors_provided_yields_random_colors(self):
+    def test_generate_from_when_no_colors_provided_yields_random_colors(
+        self, fixtures: Fixtures
+    ) -> None:
         colors = list(Color.generate_from([], 5))
 
         expected = lib.make_colors("#442082 #3cfde6 #f1c26b #30f90e #c7dd01")
         self.assertEqual(colors, expected)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_generate_from_when_not_enough_colors(self):
+    def test_generate_from_when_not_enough_colors(self, fixtures: Fixtures) -> None:
         orig_colors = lib.make_colors("#7e118f #754fc7 #835d75 #807930 #9772ea")
 
         colors = list(Color.generate_from(orig_colors, 7))
@@ -318,8 +309,7 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         )
         self.assertEqual(colors, expected)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_generate_from_more_than_enough_colors(self):
+    def test_generate_from_more_than_enough_colors(self, fixtures: Fixtures) -> None:
         orig_colors = lib.make_colors("#7e118f #754fc7 #835d75 #807930 #9772ea")
 
         colors = list(Color.generate_from(orig_colors, 3))
@@ -327,7 +317,9 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         expected = lib.make_colors("#754fc7 #7e118f #9772ea")
         self.assertEqual(colors, expected)
 
-    def test_generate_colors_when_2_input_colors_yields_gradient(self):
+    def test_generate_colors_when_2_input_colors_yields_gradient(
+        self, fixtures: Fixtures
+    ) -> None:
         orig_colors = lib.make_colors("#807930 #7e118f")
 
         colors = list(Color.generate_from(orig_colors, 7))
@@ -338,8 +330,9 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         )
         self.assertEqual(colors, expected)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_generate_from_when_needs_more_colors_than_input(self):
+    def test_generate_from_when_needs_more_colors_than_input(
+        self, fixtures: Fixtures
+    ) -> None:
         orig_colors = lib.make_colors("#7e118f #754fc7 #835d75 #7f564f")
 
         colors = list(Color.generate_from(orig_colors, 7))
@@ -349,8 +342,7 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         )
         self.assertEqual(colors, expected)
 
-    @mock.patch("larry.color.random", random.Random(1))
-    def test_randhue(self):
+    def test_randhue(self, fixtures: Fixtures) -> None:
         s, v = 59, 86
 
         c = Color.randhue(s, v)
@@ -360,44 +352,44 @@ class ColorTests(TestCase):  # pylint: disable=too-many-public-methods
         self.assertEqual(round(s), 59)
         self.assertEqual(round(v), 86)
 
-    def test_to_hsv(self):
+    def test_to_hsv(self, fixtures: Fixtures) -> None:
         h, s, v = Color("#9a59db").to_hsv()
 
         self.assertEqual(round(h), 270)
         self.assertEqual(round(s), 59)
         self.assertEqual(round(v), 86)
 
-    def test_to_hsv_black(self):
+    def test_to_hsv_black(self, fixtures: Fixtures) -> None:
         h, s, v = Color("#000000").to_hsv()
 
         self.assertEqual(round(h), 359)
         self.assertEqual(round(s), 0)
         self.assertEqual(round(v), 0)
 
-    def test_from_hsv(self):
+    def test_from_hsv(self, fixtures: Fixtures) -> None:
         hsv = (270, 59, 86)
 
         self.assertEqual(Color.from_hsv(hsv), Color("#9a59db"))
 
-    def test_hsv_h_negative(self):
+    def test_hsv_h_negative(self, fixtures: Fixtures) -> None:
         hsv = (-90, 59, 86)
 
         self.assertEqual(Color.from_hsv(hsv), Color("#9a59db"))
 
-    def test_from_hsv_with_360(self):
+    def test_from_hsv_with_360(self, fixtures: Fixtures) -> None:
         hsv = (360, 59, 86)
 
         self.assertEqual(Color.from_hsv(hsv), Color("#db5959"))
 
-    def test_hsv_gray(self):
+    def test_hsv_gray(self, fixtures: Fixtures) -> None:
         hsv = (270, 0, 86)
 
         self.assertEqual(Color.from_hsv(hsv), Color("#dbdbdb"))
 
-    def test_soften(self):
+    def test_soften(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color("#9a59db").soften(), Color("#c9a6ec"))
 
-    def test_soften_with_softness(self):
+    def test_soften_with_softness(self, fixtures: Fixtures) -> None:
         self.assertEqual(Color("#9a59db").soften(softness=0.7), Color("#dec8f4"))
 
 
