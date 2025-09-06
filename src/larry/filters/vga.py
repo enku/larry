2f77665cfac67febe285ff1dcb792ca67e8c2c4f
Albert Hopkins <marduk@letterboxes.org>
2025-09-06 12:07:38 -0500

    filters.vga: use a list comprehension

--- src/larry/filters/vga.py
+++ src/larry/filters/vga.py
@@ -7,15 +7,11 @@ from larry import Color, ColorList
 
 def cfilter(orig_colors: ColorList, config: ConfigParser) -> ColorList:
     """A blast from the past"""
-    colors: ColorList = []
     bits = config.getint("filters:vga", "bits", fallback=8)
     div = 256 / bits
 
-    for color in orig_colors:
-        red, green, blue = color
-        red = int(red // div * div)
-        green = int(green // div * div)
-        blue = int(blue // div * div)
-        colors.append(Color(red, green, blue))
-
-    return colors
+    return [
+        Color(int(red // div * div), int(green // div * div), int(blue // div * div))
+        for color in orig_colors
+        for red, green, blue in [color]
+    ]
