e25a7cf7ed016079a9ccff4d83f1fc9f664023ff
Albert Hopkins <marduk@letterboxes.org>
2025-09-06 18:55:50 -0500

    filters.colorbalance: use numpy

--- src/larry/filters/colorbalance.py
+++ src/larry/filters/colorbalance.py
@@ -2,22 +2,15 @@
 
 from configparser import ConfigParser
 
+import numpy as np
+
 from larry.color import Color, ColorList
 
 
 def cfilter(orig_colors: ColorList, _config: ConfigParser) -> ColorList:
     """Adjust the colors to achieve a more harmonious color scheme"""
-    new_colors: ColorList = []
-    size = len(orig_colors)
-    normalized = [(c.red / 255, c.green / 255, c.blue / 255) for c in orig_colors]
-    average = [sum(c[i] for c in normalized) / size for i in range(3)]
+    normalized = np.array(orig_colors) / 255
+    average = normalized.mean(axis=0)
+    adjusted = np.clip(normalized + (average - normalized) * 0.5, 0, 1) * 255
 
-    for c in normalized:
-        adjusted = [min(max(c[i] + (average[i] - c[i]) * 0.5, 0), 1) for i in range(3)]
-        new_colors.append(
-            Color(
-                int(adjusted[0] * 255), int(adjusted[1] * 255), int(adjusted[2] * 255)
-            )
-        )
-
-    return new_colors
+    return [Color.from_array(carray) for carray in adjusted]
