f9cdbe82bd634be62c1f8d76d62e50077a5bf580
Albert Hopkins <marduk@letterboxes.org>
2025-09-11 06:57:25 -0500

    tests: move timeofday tests to their own module

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-docstring
-import datetime as dt
 import importlib.metadata
 import random
 from inspect import signature
@@ -622,51 +621,6 @@ class KaleidoscopeTests(FilterTestCase):
 
         self.assertEqual(len(filtered), len(colors))
         self.assertEqual(filtered, expected)
-
-
-@params(time=["06:00:00", "9:00:00", "14:00:00", "18:00:00", "20:30:00", "22:00:00"])
-@params(
-    expected=[
-        "#640d72 #5d3f9f #684a5d #666026 #785bbb #7f753c #2db91b #2ab2ba",
-        "#710f80 #6947b3 #755369 #736c2b #8766d2 #8f8443 #33d01e #2fc8d1",
-        "#7e108f #754fc7 #835c75 #807830 #9772ea #9f934b #39e822 #34dfe9",
-        "#7e108f #754fc7 #835c75 #807830 #9772ea #9f934b #39e822 #34dfe9",
-        "#5c0c68 #553991 #604455 #5d5823 #6e53ab #746b37 #29aa18 #26a3aa",
-        "#3f0847 #3a2763 #412e3a #403c18 #4b3975 #4f4925 #1c7411 #1a6f74",
-    ]
-)
-class TimeOfDayFilters(FilterTestCase):
-    entry_point = "timeofday"
-
-    def test(self, fixtures: Fixtures) -> None:
-        config = lib.make_config("larry")
-        hour, minute, second = (int(s) for s in fixtures.time.split(":"))
-
-        with mock.patch("larry.filters.timeofday.now") as now:
-            now.return_value = dt.datetime(2025, 9, 7, hour, minute, second)
-            colors = self.filter(ORIG_COLORS, config)
-
-        self.assertEqual(
-            colors, lib.make_colors(fixtures.expected), " ".join(str(c) for c in colors)
-        )
-
-
-class TimeOfDayFilterConfigurableDaystart(FilterTestCase):
-    entry_point = "timeofday"
-
-    def test(self) -> None:
-        config = lib.make_config("larry")
-        config.add_section("filters:timeofday")
-        config.set("filters:timeofday", "midday", "9")
-
-        with mock.patch("larry.filters.timeofday.now") as now:
-            now.return_value = dt.datetime(2025, 9, 7, 9, 0, 0)
-            colors = self.filter(ORIG_COLORS, config)
-
-        expected = lib.make_colors(
-            "#7e108f #754fc7 #835c75 #807830 #9772ea #9f934b #39e822 #34dfe9"
-        )
-        self.assertEqual(colors, expected)
 
 
 class IntensifyFilterTest(FilterTestCase):
