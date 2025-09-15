2da6daa70dd5bd8349abb296e4fa06c93e5a9a17
Albert Hopkins <marduk@letterboxes.org>
2025-09-15 06:45:16 -0500

    filters.hueshift: allow specifing "random" as an amount
    When this is given the amount will be picked randomly.

--- src/larry/filters/hueshift.py
+++ src/larry/filters/hueshift.py
@@ -1,16 +1,44 @@
 """hueshift color filter"""
 
+import random
+import sys
 from configparser import ConfigParser
 
 from larry.color import Color, ColorList
 
+DEFAULT_AMOUNT = -90.0
+
 
 def cfilter(orig_colors: ColorList, config: ConfigParser) -> ColorList:
     """Shift the hues of each color by a given amount"""
-    amount = config.getfloat("filters:hueshift", "amount", fallback=-90.0)
+    amount_str = config.get("filters:hueshift", "amount", fallback="")
+    amount = get_amount(amount_str)
 
     return [
         Color.from_hsv((((h + amount) % 360), s, v))
         for color in orig_colors
         for h, s, v in [color.to_hsv()]
     ]
+
+
+def get_amount(amount_str: str, default: float = DEFAULT_AMOUNT) -> float:
+    """Return the given amount string as a float.
+
+    If the string is the empty string, the default value is returned
+
+    If the string == "random" then a random float [0, 360] is returned
+
+    If the string cannot translated to a float, a message is printed to stderr and the
+    default value is returned.
+    """
+    if amount_str == "":
+        return default
+
+    if amount_str == "random":
+        return random.random() * 360
+
+    try:
+        return float(amount_str)
+    except ValueError:
+        print(f"amount could not be parsed as float: {amount_str}", file=sys.stderr)
+        return default
--- tests/test_filters_hueshift.py
+++ tests/test_filters_hueshift.py
@@ -1,6 +1,11 @@
 """tests for the hueshift filter"""
 
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,unused-argument
+
+import io
+from contextlib import redirect_stderr
+
+from unittest_fixtures import Fixtures, given, where
 
 from . import lib
 
@@ -9,10 +14,13 @@ ORIG_COLORS = lib.make_colors(
 )
 
 
+@given(lib.random)
+@where(random__target="larry.filters.hueshift.random")
+@given(stderr=lambda _: redirect_stderr(io.StringIO()))
 class HueShiftTests(lib.FilterTestCase):
     entry_point = "hueshift"
 
-    def test(self):
+    def test(self, fixtures: Fixtures) -> None:
         config = lib.make_config("larry")
 
         colors = self.filter(ORIG_COLORS, config)
@@ -21,3 +29,29 @@ class HueShiftTests(lib.FilterTestCase):
             "#10608f #4fc7b1 #5c6283 #80305f #72ead3 #9f4b80 #e86e22 #85e934"
         )
         self.assertEqual(colors, expected)
+
+    def test_random_amount(self, fixtures: Fixtures) -> None:
+        config = lib.make_config("larry")
+        config["filters:hueshift"] = {"amount": "random"}
+
+        colors = self.filter(ORIG_COLORS, config)
+
+        expected = lib.make_colors(
+            "#8f103a #c74fb8 #83635c #468030 #ea72dc #679f4b #22e8aa #344de9"
+        )
+        self.assertEqual(colors, expected)
+
+    def test_invalid_amount_given(self, fixtures: Fixtures) -> None:
+        config = lib.make_config("larry")
+        config["filters:hueshift"] = {"amount": "bogus"}
+
+        with fixtures.stderr as stderr:
+            colors = self.filter(ORIG_COLORS, config)
+
+        expected = lib.make_colors(
+            "#10608f #4fc7b1 #5c6283 #80305f #72ead3 #9f4b80 #e86e22 #85e934"
+        )
+        self.assertEqual(colors, expected)
+        self.assertEqual(
+            stderr.getvalue(), "amount could not be parsed as float: bogus\n"
+        )
