809820bdb3409fe9c94b4906d9bd1323bde55f34
Albert Hopkins <marduk@letterboxes.org>
2025-09-28 17:00:27 -0500

    pool: add type hints

--- src/larry/pool.py
+++ src/larry/pool.py
@@ -5,11 +5,16 @@ Callers can run a function in a non async function in a thread and await the res
 
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+from typing import Callable, TypeVar, TypeVarTuple
 
 pool = ThreadPoolExecutor()
 
 
-async def run(func, *args):
+P = TypeVarTuple("P")
+R = TypeVar("R")
+
+
+async def run(func: Callable[[*P], R], *args: *P) -> R:
     """Apply the givne func/args to the executor pool and yield the result"""
     loop = asyncio.get_event_loop()
 
