4beb8a939f5d840c3864cb1ad78c1e8e6cede8a7
Albert Hopkins <marduk@letterboxes.org>
2025-10-04 15:33:43 -0500

    filters.none: fix docstring

--- src/larry/filters/none.py
+++ src/larry/filters/none.py
@@ -8,6 +8,6 @@ from larry import ColorList
 def cfilter(orig_colors: ColorList, _config: ConfigParser) -> ColorList:
     """A NO-OP filter
 
-    This is an filter that simply returns the original colors.
+    This is a filter that simply returns the original colors.
     """
     return orig_colors
